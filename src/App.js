0d1c31e2c7b9a03e942c132139384ce6a02d06ef
David Holmes <david.holmes0712@gmail.com>
2018-01-19 21:25:22 +1100

    Readded "exact path" to Home route

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
         <div className="App">
           <NavBar isAuthenticated={signedIn}/>
           <Switch>
-            <Route path="/" render={()=><HomePage isAuthenticated={signedIn}/>}/>
+            <Route exact path="/" render={()=><HomePage isAuthenticated={signedIn}/>}/>
             <Route exact path="/sign-in" component={SignInPage}/>
             <Route exact path="/sign-up" component={SignUpPage}/>
           </Switch>
