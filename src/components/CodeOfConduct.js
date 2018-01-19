6cac8e44f5108d02761a651c6bb9d2cec875f7c6
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-01-19 13:33:09 +1100

    added more content to code of conduct

--- src/components/CodeOfConduct.js
+++ src/components/CodeOfConduct.js
@@ -32,10 +32,51 @@ function CodeOfConduct (){
                         <br></br>
                         <li>High fives are appropriate and a good way to affirm a young person craving physical touch.</li>
                     </ul>
-                <br></br>
-                <li>Any contact with young people part of Ignite must be above reproach. When contact is made outside the program it is paramount that the following points are adhered to. </li>              
-
-                
+            <br></br>
+                <li>Any contact with young people part of Ignite must be above reproach. When contact is made outside the program it is paramount that the following points are adhered to. </li>
+                <ul>
+                    <li>Social media must only be used as a means to communicate information or to provide positive feedback to a group. Do not post opinions of potential controversy. </li>
+                    <br></br>
+                    <li>Private messages must be kept minimal. No messages to opposite sex unless discussed with supervisor. </li>
+                    <br></br>
+                    <li>Understand the digital footprint. Remember what is put on Facebook may be used against you. Do not post anything parents/ignite directors or school principal would disapprove including their personal inbox.</li>
+                    <br></br>
+                    <li>If you are meeting with a young person outside of Ignite program, this needs to be approved by your supervisor and their parents. </li>
+            <br></br>
+                </ul>             
+                <li>When driving a young person, all road rules must be obeyed. If driving young people home, be careful who is the last person you drop off (eg not person of opposite sex and do not sit in the car and talk). Do however wait until they are safely inside before driving off. Parent consent should be given before driving and you must have comprehensive insurance. </li>
+            <br></br>
+                <li>Respecting all ignite and school property and equipment and use properly. </li> 
+            <br></br>
+                <li>Arrive on time (eg before starting time) and be prepared for the days activities.</li>
+            <br></br>
+                <li>Dress appropriately and be free from the influence or after effects of illicit drugs or alcohol. No smoking during programs. </li> 
+            <br></br>
+                <li>Do your tasks and follow expectations to the best of your ability. </li> 
+            <br></br>
+                <li>Understanding the partnership with Keysborough College is a privilege and not a right. We are to be above reproach in all matters when working at the school and with the students outside of school.  </li>
+            <br></br> 
+                <h1>Next Page</h1> 
+            <br></br>
+                <li>All leaders to have a supervisor or someone (in a leadership position) they can report to, this is a two way relationship. Ignite creates a culture where it is low control but high accountability. Remain open and honest in all communication and feedback, seeking resolution and growth in all circumstances. </li>
+            <br></br>
+                <li>Follow good confidentiality principles. This includes. </li>
+            <br></br>
+                    <ul>
+                        <li>Never promise 100% confidentiality.</li>
+                        <li>Do not gossip or share confidential information to friends, peers or others in the program except for your supervisor. </li>
+                        <li>Inform a young person that if you believe they or someone is in danger, that you must pass this information on (to supervisor or directly to DHS)</li>
+                        <li>Report to Ignite staff if you have a concern for a young person.</li>
+                    </ul>
+            <br></br>
+                <li>Be aware of potential risks and health issues of participants and report dangers to your supervisor. </li>
+            <br></br>
+                <li>Encourage.  If constructive feedback is required, use Sandwich Principal - “Encourage - Constructive Feedback - Encourage” </li>
+            <br></br>
+                <li>Ignite is passionate about building into all leaders. Each leader is encouraged to engage in a faith community and/or a mentor relationship for personal growth. All volunteers are expected to carry out some relevant training through out the year. </li>
+            <br></br>
+                <li>Ignite knows leaders can offer a variety of talents and resources. Leadership is seen to be anything that influence a young person, therefore driving a car is seen as an opportunity to lead, not just in ‘how’ you drive but the conversation and relationships that are built during this time. Please don’t ever underestimate your influence on a young person no matter how small you feel your input is. </li>
+            <br></br>                
             </ol>
          </div>
           
