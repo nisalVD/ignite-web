914a394289a8d33aa701e9743ee4daf8dfb5ced1
nisalVD <nisalvd@gmail.com>
2018-01-24 20:29:13 +1100

    add link to add question in adminmodileview

--- src/components/AdminModuleView.js
+++ src/components/AdminModuleView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
+import Button from 'material-ui/Button';
 import { findModule } from '../api/module'
 
 class AdminModuleView extends Component {
@@ -18,8 +19,12 @@ class AdminModuleView extends Component {
     const {currentModuleData} = this.state
     return (
       <div className="admin-module-data-view">
-      <h1>Module Page View</h1>
-      <br/>
+        <h1>Module Page View</h1>
+        <div className="aq-button-right">
+        <Button raised color="primary" component={Link} to={`/admin/module/${currentModuleData._id}/questions`}>
+          Add Questions
+        </Button>
+        </div>
         <h1>{currentModuleData.name}</h1>
         <div dangerouslySetInnerHTML={{ __html: currentModuleData.content}}/>
       </div>
