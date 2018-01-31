209cc9314646e84f1e76ab7a56d99f433c45f2c4
nisalVD <nisalvd@gmail.com>
2018-01-31 14:39:55 +1100

    made feed a proper component

--- src/HomePage/Feed.js
+++ src/HomePage/Feed.js
@@ -3,15 +3,17 @@ import './Feed.css'
 
 const Module = ({
   color,
-  // heading,
-  // content,
-  // data
+  heading,
+  content,
+  date
 }) => {
   return (
     <div className={`feed-box feed-${color}`}>
-      <h1 className="feed-heading">Heading <span className="date-span">Date: {'/02/01/1995'}</span></h1>
+      <h1 className="feed-heading">{heading}
+        <span className="date-span">Date: {date}</span>
+      </h1>
       <div className="feed-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis nibh scelerisque elit ullamcorper luctus et et neque. Phasellus nec turpis maximus, rhoncus odio maximus, mollis ante. Maecenas justo nulla, mollis in lacinia a, dignissim quis massa. Etiam rhoncus commodo justo quis vulputate. Suspendisse eu commodo velit. Curabitur quis turpis eu diam finibus vehicula quis ac libero. Ut massa risus, eleifend in massa in, sollicitudin dignissim augue. Sed dictum odio eget velit ultrices molestie. 
+        {content}
       </div>
     </div>
   )
