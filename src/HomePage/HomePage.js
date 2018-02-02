6c523b1ba4d2ca08d67be4345c81cc3d2ea78dc7
nisalVD <nisalvd@gmail.com>
2018-02-02 19:58:02 +1100

    used a raised button changet text to white

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Button from 'material-ui/Button';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
 
 import { listFeeds } from '../api/feed'
 import Feed from './Feed'
 import { Link } from 'react-router-dom'
 import './HomePage.css'
+
 class HomePage extends Component {
   state = {
     feedData: null
@@ -19,27 +22,34 @@ class HomePage extends Component {
   render () {
     const { feedData } = this.state
     return (
-      <div>
-        <div className="bg-image">
-          <Button raised color="primary" className="homepage-module-button" component={Link} to="/modules">
-            Module
-          </Button>
-        </div>
+      <MuiThemeProvider theme={theme}>
         <div>
-            { feedData && 
-            feedData.reverse().map((feed, idx) => {
-              let color = 'primary'
-              if (idx%2 == 0) {
-                color = 'secondary'
-              }
-                return <Feed key={feed._id} heading={feed.heading} color={color} content={feed.content} date={feed.timePosted}/>
-              }
-            )
-          }
+          <div className="bg-image">
+            <Button raised color="primary" className="homepage-module-button" component={Link} to="/modules">
+              Module
+            </Button>
+          </div>
+          <div>
+              { feedData && 
+              feedData.reverse().map((feed, idx) => {
+                let color = 'primary'
+                if (idx%2 == 0) {
+                  color = 'secondary'
+                }
+                  return <Feed key={feed._id} heading={feed.heading} color={color} content={feed.content} date={feed.timePosted}/>
+                }
+              )
+            }
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#FF7115' } 
+  },
+});
 
 export default HomePage
