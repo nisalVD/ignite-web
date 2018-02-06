80b1f8312bcfa5e338ea731562fae1a689618893
nisalVD <nisalvd@gmail.com>
2018-02-06 23:56:20 +1100

    add export and changed url as it was changed in backend

--- src/api/updateUser.js
+++ src/api/updateUser.js
@@ -1,6 +1,8 @@
 import api from './init'
 
 export function changePassword(oldPassword, newPassword) {
-  return api.patch('/auth/update', {oldPassword, newPassword})
+  return api.patch('/user/password/update', {oldPassword, newPassword})
     .then(res => res.data)
 }
+
+export default api
