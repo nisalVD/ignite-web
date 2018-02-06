313f02c64a5ca217797b791fad6ca41d2ed0af5a
nisalVD <nisalvd@gmail.com>
2018-02-06 23:58:31 +1100

    added update details api

--- src/api/updateUser.js
+++ src/api/updateUser.js
@@ -1,7 +1,13 @@
 import api from './init'
+import { getUserData } from './adminData';
 
 export function changePassword(oldPassword, newPassword) {
   return api.patch('/user/password/update', {oldPassword, newPassword})
+    .then(res => res.data)
+}
+
+export function updateDetails(userDetails) {
+  return api.patch('/user/details/update', {userDetails})
     .then(res => res.data)
 }
 
