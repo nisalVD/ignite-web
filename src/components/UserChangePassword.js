79df94f12edc27d197c09467c5a5a217994cfd33
nisalVD <nisalvd@gmail.com>
2018-02-09 21:52:25 +1100

    add basic password change form just gotta fix error when change pw

--- src/components/UserChangePassword.js
+++ src/components/UserChangePassword.js
@@ -2,27 +2,51 @@ import React, { Component } from 'react'
 import { changePassword } from '../api/updateUser'
 
 class UserChangePassword extends Component {
+  state = {
+    success: null
+  }
+    handleChangePassword = (event) => {
+      event.preventDefault()
+      const {elements} = event.target
+      const newPassword = elements.newPassword.value
+      const oldPassword = elements.oldPassword.value
+      changePassword(oldPassword, newPassword)
+        .then(response => {
+          this.setState({success: response.success})
+        })
+        .catch(error => console.log(error))
+    }
   render () {
+    const {success} = this.state
+    console.log('success', success)
     return (
-      <form>
-        <label>
-          OldPassword:
-          <input 
-            type="text"
-            name="oldPassword"
-            />
-        </label>
-        <br/>
-        <label>
-          NewPassword:
-          <input 
-            type="text"
-            name="oldPassword"
-            />
-        </label>
-        <br/>
-        <button className="button">Submit</button>
-      </form>
+      <div>
+        {
+          success === false && <p>Incorrect Password</p>
+        }
+        {
+          success === true && <p>Password Changed</p>
+        }
+        <form onSubmit={this.handleChangePassword}>
+          <label>
+            OldPassword:
+            <input 
+              type="text"
+              name="oldPassword"
+              />
+          </label>
+          <br/>
+          <label>
+            NewPassword:
+            <input 
+              type="text"
+              name="newPassword"
+              />
+          </label>
+          <br/>
+          <button className="button">Submit</button>
+        </form>
+      </div>
     )
   }
 }
