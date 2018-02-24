e6107d99444df7e39f063ab504b013462c304b23
nisalVD <nisalvd@gmail.com>
2018-02-24 23:28:52 +1100

    add password match check that checks before form

--- src/components/UserChangePassword.js
+++ src/components/UserChangePassword.js
@@ -4,10 +4,12 @@ import { changePassword } from "../api/updateUser"
 class UserChangePassword extends Component {
   state = {
     error: null,
-    passwordChanged: false
+    passwordChanged: false,
+    newPassword: null,
+    newPasswordConfirm: null
   }
 
-  handleChangePassword = event => {
+  handleChangePasswordForm = event => {
     event.preventDefault()
     const { elements } = event.target
     const newPassword = elements.newPassword.value
@@ -32,13 +34,22 @@ class UserChangePassword extends Component {
         })
     }
   }
+  handleNewPasswordChange = (passwordType, event) => {
+    const { newPassword, newPasswordConfirm } = this.state
+    this.setState({ [passwordType]: event.target.value })
+    if (newPassword !== newPasswordConfirm) {
+      this.setState({ error: "passwords don't match" })
+    }
+  }
+
   render() {
     const { error, passwordChanged } = this.state
+    const { newPassword, newPasswordConfirm } = this.state
     return (
       <div>
         {passwordChanged && <p>Password Changed</p>}
         <p>{error && error}</p>
-        <form onSubmit={this.handleChangePassword}>
+        <form onSubmit={this.handleChangePasswordForm}>
           <label>
             OldPassword:
             <input type="password" name="oldPassword" />
@@ -46,11 +57,22 @@ class UserChangePassword extends Component {
           <br />
           <label>
             NewPassword:
-            <input type="password" name="newPassword" />
+            <input
+              type="password"
+              name="newPassword"
+              onChange={this.handleNewPasswordChange.bind(this, "newPassword")}
+            />
           </label>
           <label>
             Confirm NewPassword:
-            <input type="password" name="newPasswordConfirm" />
+            <input
+              type="password"
+              name="newPasswordConfirm"
+              onChange={this.handleNewPasswordChange.bind(
+                this,
+                "newPasswordConfirm"
+              )}
+            />
           </label>
           <br />
           <button className="button">Submit</button>
