7f39b11a9729e49df0b58de4aa49495421c301d2
nisalVD <nisalvd@gmail.com>
2018-02-24 23:12:37 +1100

    made password input type into password

--- src/components/UserChangePassword.js
+++ src/components/UserChangePassword.js
@@ -1,72 +1,60 @@
-import React, { Component } from 'react'
-import { changePassword } from '../api/updateUser'
+import React, { Component } from "react"
+import { changePassword } from "../api/updateUser"
 
 class UserChangePassword extends Component {
   state = {
-    error : null,
+    error: null,
     passwordChanged: false
   }
-    handleChangePassword = (event) => {
-      event.preventDefault()
-      const {elements} = event.target
-      const newPassword = elements.newPassword.value
-      const newPasswordConfirm = elements.newPasswordConfirm.value
-      const oldPassword = elements.oldPassword.value
-      if (newPassword !== newPasswordConfirm) {
-        this.setState({error: "passwords don't match"})
-      }
-      else {
-        changePassword(oldPassword, newPassword)
-          .then(response => {
-            if(response.success !== 'success'){
-              this.setState({error: 'Incorrect Password'})
-            }
-          })
-          .then(() => {
-            const later = (delay, value) =>
-            new Promise(resolve => setTimeout(resolve, delay, value));
-            later(3000, null)
-              .then(error => this.setState({error}))
-          })
-          .catch(error => {
-            this.setState({passwordChanged: error.response.data.sucess})
-          })
-      }
+
+  handleChangePassword = event => {
+    event.preventDefault()
+    const { elements } = event.target
+    const newPassword = elements.newPassword.value
+    const newPasswordConfirm = elements.newPasswordConfirm.value
+    const oldPassword = elements.oldPassword.value
+    if (newPassword !== newPasswordConfirm) {
+      this.setState({ error: "passwords don't match" })
+    } else {
+      changePassword(oldPassword, newPassword)
+        .then(response => {
+          if (response.success !== "success") {
+            this.setState({ error: "Incorrect Password" })
+          }
+        })
+        .then(() => {
+          const later = (delay, value) =>
+            new Promise(resolve => setTimeout(resolve, delay, value))
+          later(3000, null).then(error => this.setState({ error }))
+        })
+        .catch(error => {
+          this.setState({ passwordChanged: error.response.data.sucess })
+        })
     }
-  render () {
-    const {error, passwordChanged} = this.state
-    console.log('error', error)
-    console.log('password Changed', this.state.passwordChanged)
+  }
+  render() {
+    const { error, passwordChanged } = this.state
+    console.log("error", error)
+    console.log("password Changed", this.state.passwordChanged)
     return (
       <div>
-        { passwordChanged &&
-          <p>Password Changed</p>
-        }
+        {passwordChanged && <p>Password Changed</p>}
         <p>{error && error}</p>
         <form onSubmit={this.handleChangePassword}>
           <label>
             OldPassword:
-            <input 
-              type="text"
-              name="oldPassword"
-              />
+            <input type="password" name="oldPassword" />
           </label>
-          <br/>
+          <br />
           <label>
             NewPassword:
-            <input 
-              type="text"
-              name="newPassword"
-              />
+            <input type="password" name="newPassword" />
           </label>
           <label>
             Confirm NewPassword:
-            <input 
-              type="text"
-              name="newPasswordConfirm"
-              />
+            <input type="password" name="newPasswordConfirm" />
           </label>
-          <br/>
+          <br />
           <button className="button">Submit</button>
         </form>
       </div>
