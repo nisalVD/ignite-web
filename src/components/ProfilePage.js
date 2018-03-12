f34e49a26205b2d2bb5a7a7d5ba6384790102ccc
nisalVD <nisalvd@gmail.com>
2018-03-12 21:25:30 +1100

    add sign out button in profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,94 +1,113 @@
-import React, { Component } from 'react'
-import LetterAvatar from './Avatar';
-import UpdateDetailsButton from './UpdateDetailsButton'
-import CheckList from './CheckList'
-import { Link } from "react-router-dom"
-import { listModules } from '../api/module'
-import { checkMarking } from '../api/question'
+import React, {Component} from "react"
+import LetterAvatar from "./Avatar"
+import UpdateDetailsButton from "./UpdateDetailsButton"
+import CheckList from "./CheckList"
+import {Link} from "react-router-dom"
+import {listModules} from "../api/module"
+import {checkMarking} from "../api/question"
 
 class ProfilePage extends Component {
   state = {
     currentUserMarkingData: null,
-    modules: null
+    modules: null,
   }
 
   componentDidMount() {
-
-    checkMarking(this.props.decodedToken.sub) 
-    .then(currentUserMarkingData => {
-      this.setState({currentUserMarkingData})
-    })
-    .catch(error => console.log(error))
+    checkMarking(this.props.decodedToken.sub)
+      .then(currentUserMarkingData => {
+        this.setState({currentUserMarkingData})
+      })
+      .catch(error => console.log(error))
 
     listModules()
-    .then(modules => this.setState({modules}))
-    .catch(error => console.log(error))
+      .then(modules => this.setState({modules}))
+      .catch(error => console.log(error))
   }
 
   isModuleCompleted(module) {
     const {currentUserMarkingData} = this.state
-    const mappedMarking = currentUserMarkingData && currentUserMarkingData.reduce((acc,next) => {
-      if (next.module === module._id){
-        acc.push(next)
-      }
-      return acc
-    },[])
-    function isEveryTrue(element){
+    const mappedMarking =
+      currentUserMarkingData &&
+      currentUserMarkingData.reduce((acc, next) => {
+        if (next.module === module._id) {
+          acc.push(next)
+        }
+        return acc
+      }, [])
+    function isEveryTrue(element) {
       return element.correct === true
     }
     let isCorrect = false
-    if (!!mappedMarking){
-      if(mappedMarking.length > 0) {
+    if (!!mappedMarking) {
+      if (mappedMarking.length > 0) {
         isCorrect = mappedMarking.every(isEveryTrue)
       } else {
         isCorrect = false
       }
     }
     return isCorrect
- }
+  }
 
-  render () {
+  render() {
     const {modules} = this.state
-        return (
-            <div>
-               <div className="profile-page">
-                    <div className="profile-page-div">
-                        <LetterAvatar/>
-                        <br/>
-                        {this.props.decodedToken.email}
-                        <br /><br />
-                        <button className="sign-out-button" onClick={(event => this.props.onSignOut())}>SIGN OUT</button>
-                        <br /><br />
-                        {/* <UpdateDetailsButton onUpdateUser={onUpdateUser}/> */}
-                        <br /><br />
-                        <div className="modules-container">
-                            <div className="profile-left">
-                                <h3>Modules Completed: </h3>
-                                { modules && modules.map(module => {
-                                  return (
-                                    <div>
-                                    {this.isModuleCompleted.bind(this, module)() && 
-                                      <p>{module.name}</p>
-                                    }
-                                    </div>
-                                  )
-                                })
-                                }
-                            </div>
-                            <div className="profile-right">                        
-                              <Link to={`./modules`}><button type="button" className="modules-button">MODULES</button></Link>
-                            </div>
-                        </div>
-                        <br /><br />
-                              <div className="induction-box">
-                              <div className="induction-instructions">
-                                In order to qualify to volunteer at Ignite you must complete the list of requirements.
-                                <br /> 
-                                Please ensure you have:
-                              </div>
-                                <br />
-                              <div className="induction-list">
-                                {/* <form className="induction-form">
+    return (
+      <div>
+        <div className="profile-page">
+          <div className="profile-page-div">
+            <LetterAvatar />
+            <br />
+            {this.props.decodedToken.email}
+            <br />
+            <br />
+            <button
+              className="sign-out-button"
+              onClick={event => this.props.onSignOut()}>
+              SIGN OUT
+            </button>
+            <br />
+            <br />
+            <Link className="sign-out-button" to="/user/change-password">
+              Change Password
+            </Link>
+            <br />
+            <br />
+            {/* <UpdateDetailsButton onUpdateUser={onUpdateUser}/> */}
+            <br />
+            <br />
+            <div className="modules-container">
+              <div className="profile-left">
+                <h3>Modules Completed: </h3>
+                {modules &&
+                  modules.map(module => {
+                    return (
+                      <div>
+                        {this.isModuleCompleted.bind(this, module)() && (
+                          <p>{module.name}</p>
+                        )}
+                      </div>
+                    )
+                  })}
+              </div>
+              <div className="profile-right">
+                <Link to={`./modules`}>
+                  <button type="button" className="modules-button">
+                    MODULES
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="induction-box">
+              <div className="induction-instructions">
+                In order to qualify to volunteer at Ignite you must complete the
+                list of requirements.
+                <br />
+                Please ensure you have:
+              </div>
+              <br />
+              <div className="induction-list">
+                {/* <form className="induction-form">
                                   <label>
                                     <input className="induction-radio" type="radio" value="option1" />
                                     Completed Modules
@@ -108,20 +127,20 @@ class ProfilePage extends Component {
                                   <br /><br />
                                   <input type="reset"/>
                               </form> */}
-                              <CheckList/>
-                              </div>
-                              </div>
-                        <br /><br />
-                            <div className="support-div">
-                            Need help?
-                            <br />
-                            Contact us at ignite@gmail.com or on 040909090
-                            </div>
-                        <div className="admin-page-padder"/>
-                    </div>
-
-                </div>
+                <CheckList />
+              </div>
             </div>
+            <br />
+            <br />
+            <div className="support-div">
+              Need help?
+              <br />
+              Contact us at ignite@gmail.com or on 040909090
+            </div>
+            <div className="admin-page-padder" />
+          </div>
+        </div>
+      </div>
     )
   }
 }
