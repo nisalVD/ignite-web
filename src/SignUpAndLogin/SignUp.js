b6b38f4ba98d7f557b9798dde09732b0696b986d
nisalVD <nisalvd@gmail.com>
2018-04-26 03:52:09 +1000

    add button to verify if everything is filled and input is appropriate

--- src/SignUpAndLogin/SignUp.js
+++ src/SignUpAndLogin/SignUp.js
@@ -1,97 +1,170 @@
 import React, { Component } from 'react'
+import {CircularProgress, Button} from 'material-ui'
 import './SignUp.css'
 import {isEmailValid} from '../api/auth.js'
 
 class SignUp extends Component {
   state = {
     email: '',
-    isEmailValid: null,
-    selectedInputValue: null
+    password: '',
+    confirmPassword: '',
+    emailValid: null,
+    loadingEmailValid: false,
+    selectedInputValue: {
+      email: false,
+      password: false,
+      confirmPassword: false
+    }
   }
 
-  checkEmailIsValid = () => {
-    this.setState({selectedInputValue: null})
-    const {email } = this.state
-    isEmailValid(email)
-      .then(({message}) => {
-        this.setState({isEmailValid: message})
-      })
-  }
-
-  handleInputFocus = (selectedInputValue) => {
-    this.setState({selectedInputValue})
-  }
 
   handleInputPlaceholder = (inputName) => {
     const {selectedInputValue} = this.state
     if (selectedInputValue === inputName) {
       return ""
     } else {
-      return inputName
+      return this.convertCamelCaseToSentenceCase(inputName)
     }
   }
 
-  handleBlurInputValue = () => {
-    this.setState({selectedInputValue: null})
+  // onFocus
+  handleInputFocus = (inputName) => {
+    const selectedInputValue = {
+      ...this.state.selectedInputValue,
+      [inputName]: true
+    }
+
+    this.setState({selectedInputValue})
   }
+
+  // onBlur
+  handleInputBlur = (inputName) => {
+    const selectedInputValue = {
+      ...this.state.selectedInputValue,
+      [inputName]: false
+    }
+    this.setState({selectedInputValue})
+  }
+
+  // On Email Blur
+  checkEmailIsValid = (inputName) => {
+    const {email} = this.state
+    const emailValid = /^.+@.+$/.test(email.trim())
+    if (emailValid === false) {
+      this.setState({emailValid: false})
+      return
+    }
+
+    this.setState({loadingEmailValid: true})
+    isEmailValid(email)
+      .then(({message}) => {
+        const selectedInputValue = {
+          ...this.state.selectedInputValue,
+          [inputName]: true
+        }
+        this.setState({loadingEmailValid: false, emailValid: message, selectedInputValue})
+      })
+
+  }
+
 
   handleLabelText = (inputName) => {
     const {selectedInputValue} = this.state
-
-    if (inputName === selectedInputValue) {
-      return inputName
-    } else {
-      return '_____'
+    if (selectedInputValue[inputName] === true){
+      return this.convertCamelCaseToSentenceCase(inputName)
     }
   }
 
+  handleEmailValidLabel = () => {
+    const {emailValid} = this.state
+    if (emailValid) {
+      return 'email-valid-sucess'
+    }
+    if (emailValid === false) {
+      return 'email-valid-fail'
+    }
+  }
+
+  // Helper function
+  convertCamelCaseToSentenceCase = (input)  => {
+    const parsedInput = input.replace( /([A-Z])/g, " $1" );
+    return parsedInput.charAt(0).toUpperCase() + parsedInput.slice(1)
+  }
+
+  handleButtonDisabled = () => {
+    const {email, password, confirmPassword, emailValid} = this.state
+    if(emailValid && email && password && confirmPassword){
+      return false
+    } else {
+      return true
+    }
+  }
 
   render() {
 
-    const {email, isEmailValid, selectedInputValue} = this.state
+    const {email, password, confirmPassword, emailValid, selectedInputValue, loadingEmailValid} = this.state
+    // console.log('emailValid', emailValid)
+    // console.log('selectedInputValue', selectedInputValue)
     return (
       <div className="sign-up-form-container">
         <div className="sign-up-container">
           <h1>Sign Up</h1>
-          <label className="sign-up-label-style">
-            {this.handleLabelText('Email')}
+          <label className={`sign-up-label-style ${this.handleEmailValidLabel()}`} >
+            {this.handleLabelText('email')}
           </label>
+          {
+            loadingEmailValid ?
+              <div className="loading-email-valid-container">
+                <CircularProgress size={30} />
+              </div>
+            :
           <input
-            ref={this.handleInputRef}
             className="sign-up-input"
-            onFocus={this.handleInputFocus.bind(this, 'Email')}
+            onFocus={this.handleInputFocus.bind(this, 'email')}
             type="email"
             name="email"
-            placeholder={this.handleInputPlaceholder('Email')}
+            placeholder={this.handleInputPlaceholder('email')}
             onChange={(e)=> this.setState({email: e.target.value})}
-            onBlur={this.checkEmailIsValid}
+            onBlur={this.checkEmailIsValid.bind(this, 'email')}
             value={email}
           />
-          <br/>
-          <br/>
-
-          <label className="sign-up-label-style">{this.handleLabelText('Password')}</label>
+          }
+          {emailValid === null && <div className="empty-div" />}
+          {emailValid === true && <div className="email-valid-sucess">Valid Email</div>}
+          {emailValid === false && <div className="email-valid-fail">Email is invalid or Already in Use</div>}
+          <label className="sign-up-label-style">{this.handleLabelText('password')}</label>
           <input
             className="sign-up-input"
-            onFocus={this.handleInputFocus.bind(this, 'Password')}
-            onBlur={this.handleInputFocus}
-            placeholder={this.handleInputPlaceholder('Password')}
+            onFocus={this.handleInputFocus.bind(this, 'password')}
+            onBlur={this.handleInputBlur.bind(this, 'password')}
+            placeholder={this.handleInputPlaceholder('password')}
+            onChange={(e)=> this.setState({password: e.target.value})}
             type="password"
             name="password"
+            value={password}
           />
           <br/>
           <br/>
 
-          <label className="sign-up-label-style">{this.handleLabelText('Confirm Password')}</label>
+          <label className="sign-up-label-style">{this.handleLabelText('confirmPassword')}</label>
           <input
-            onFocus={this.handleInputFocus.bind(this, 'Confirm Password')}
-            onBlur={this.handleInputFocus}
-            placeholder={this.handleInputPlaceholder('Confirm Password')}
+            onFocus={this.handleInputFocus.bind(this, 'confirmPassword')}
+            onBlur={this.handleInputBlur.bind(this, 'confirmPassword')}
+            placeholder={this.handleInputPlaceholder('confirmPassword')}
             className="sign-up-input"
             type="password"
             name="confirmPassword"
+            onChange={(e)=> this.setState({confirmPassword: e.target.value})}
+            value={confirmPassword}
           />
-
+        </div>
+        <div className="sign-up-button-container">
+          <Button
+            disabled={this.handleButtonDisabled()}
+            raised
+            color="primary"
+          >Next
+          </Button>
         </div>
       </div>
     )
